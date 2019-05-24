3312863ff54c29bd1235256c25b47c16182c4a34
sangram Keshari Behera <sangram.behera@apisero.com>
2019-05-24 15:33:27 +0530

    Updated copyright

--- src/main/java/com/mulesoft/connectors/blend/internal/error/exception/ResponseValidator.java
+++ src/main/java/com/mulesoft/connectors/blend/internal/error/exception/ResponseValidator.java
@@ -1,4 +1,6 @@
-
+/**
+ * (c) 2003-2019 MuleSoft, Inc. The software in this package is published under the terms of the Commercial Free Software license V.1 a copy of which has been included with this distribution in the LICENSE.md file.
+ */
 package com.mulesoft.connectors.blend.internal.error.exception;
 
 
